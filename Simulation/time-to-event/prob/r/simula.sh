8146bfbcdb9e7e4742b891cdf50da6721bd3401f
Marlena Bannick <mnorwood@rhino01.fhcrc.org>
2021-05-20 12:07:47 -0700

    New ppartition

--- Simulation/time-to-event/prob/r/simula.sh
+++ Simulation/time-to-event/prob/r/simula.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1 #not required if only 1 thread as 1 thread per task is the default
 #SBATCH --mem=4G
-#SBATCH --partition=curie-cpu
+#SBATCH --partition=campus-new
 #SBATCH --array=1-500
 echo "$SLURM_ARRAY_TASK_ID"
 
