e439f553fb8e678f9685abcc218310986c2401f3
Marlena Bannick <mnorwood@uw.edu>
2021-05-20 14:53:02 -0700

    Updated memory usage

--- Simulation/time-to-event/prob/r/simula.r
+++ Simulation/time-to-event/prob/r/simula.r
@@ -2,7 +2,12 @@
 source('~/COVID-19-RCT-STAT-TOOLS/Simulation/time-to-event/prob/r/code.r')
 task_id = Sys.getenv("SLURM_ARRAY_TASK_ID")
 ## sink(file = paste('log', task_id, '.txt', sep=''))
-try(assign(paste('r', task_id, sep = ''), try(funslave(as.numeric(task_id)))))
+nn <- paste('/fh/scratch/delete30/gao_f/mbannick/tte-output/prob/out/out', task_id, '.rda', sep = '')
 ## sink()
-save.image(paste('/fh/scratch/delete30/gao_f/mbannick/tte-output/prob/out/out', task_id, '.rda', sep = ''))
+if(!file.exists(nn)){
+  try(assign(paste('r', task_id, sep = ''), try(funslave(as.numeric(task_id)))))
+  save.image(paste('/fh/scratch/delete30/gao_f/mbannick/tte-output/prob/out/out', task_id, '.rda', sep = ''))
+} else {
+  print("file already exists.")
+}
 quit('no')
--- Simulation/time-to-event/prob/r/simula.sh
+++ Simulation/time-to-event/prob/r/simula.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=array_job
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1 #not required if only 1 thread as 1 thread per task is the default
-#SBATCH --mem=4G
+#SBATCH --cpus-per-task=2 #not required if only 1 thread as 1 thread per task is the default
+#SBATCH --mem=8G
 #SBATCH --partition=campus-new
 #SBATCH --array=1-500
 echo "$SLURM_ARRAY_TASK_ID"
